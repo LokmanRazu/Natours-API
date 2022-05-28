bf4329bd794f0517e7908511a21409a95d21ce0b
Lokman Hossain Razu <89483463+LokmanRazu@users.noreply.github.com>
2022-05-28 23:46:11 +0600

    virtuals properties

--- model/tourMoel.js
+++ model/tourMoel.js
@@ -29,9 +29,17 @@ const tourSchema = new mongoose.Schema({
         type:String,
         required:true
     }
+},{
+    toJSON: { virtuals:true }, toObject: { virtuals:true }      // For Virtuals Properties
 })
 
 // Mongoose Document Middleware: only exicute before .save() and .create()
+
+// Virtual Properties
+tourSchema.virtual('sinceWeeks').get( function(){
+    return this.since/7
+})
+
 // Slugify for unique any properties
 tourSchema.pre('save', function(next){
 this.slug = slugify(this.name, { lower:true })
