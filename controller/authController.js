58dc4299991534f8a675225d4b35864a52d50717
Lokman Hossain Razu <89483463+LokmanRazu@users.noreply.github.com>
2022-06-05 23:34:56 +0600

    Login discription

--- controller/authController.js
+++ controller/authController.js
@@ -24,22 +24,25 @@ exports.signup = async (req, res, next)=>{
 };
 
 exports.login = async (req,res,next)=>{
-
+  // Does user exist
   const { email, password } = req.body
   if(!email || !password){
     return next(new appError('Incorrect email or password', 401))
   };
   try{
+    // Does user { email,password } exist in DB
     let user = await User.findOne({ email }).select('password')
     if(!user){
       return next(new appError('invalid email',401))
     }
+    // Match the password
     let match =await bcrypt.compare(password, user.password)
     if(!match){
         return res.json({
             message:'Invalid Credential'
         })
       }
+      // Genarate the JWT Token
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET ,{ expiresIn:process.env.JWT_EXPIRES_IN });
       res.status(200).json({
         status:'sucess',
