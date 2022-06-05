eb42aafab68592a1a65e330cd222e1844bd17ab6
Lokman Hossain Razu <89483463+LokmanRazu@users.noreply.github.com>
2022-06-05 23:29:39 +0600

    JWT Token Genarte

--- controller/authController.js
+++ controller/authController.js
@@ -9,13 +9,9 @@ exports.signup = async (req, res, next)=>{
         let user = new User({
           name,email,password,passwordConfirm
         })
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET ,{ expiresIn:process.env.JWT_EXPIRES_IN });
         await user.save()
         res.status(201).json({
           status:"success",
-          jwtToken:{
-            token
-          },
           data:{
             user
           }
@@ -28,7 +24,7 @@ exports.signup = async (req, res, next)=>{
 };
 
 exports.login = async (req,res,next)=>{
-  let token =''
+
   const { email, password } = req.body
   if(!email || !password){
     return next(new appError('Incorrect email or password', 401))
@@ -44,6 +40,7 @@ exports.login = async (req,res,next)=>{
             message:'Invalid Credential'
         })
       }
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET ,{ expiresIn:process.env.JWT_EXPIRES_IN });
       res.status(200).json({
         status:'sucess',
         data:{
