5665349086073b7f8c9ebcb52ac595eef2a129d4
Lokman Hossain Razu <89483463+LokmanRazu@users.noreply.github.com>
2022-06-08 02:42:15 +0600

    is user exist

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,7 @@
 const appError = require('../utils/error');
+const User = require('../model/userModel')
 const { promisify } = require('util')
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
 exports.protect = async (req,res,next)=>{
 try{
@@ -9,14 +10,18 @@ try{
         if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
         token = req.headers.authorization.split(' ')[1];
         }
-        console.log(`I am token:- ${token}`)
         if(!token){
             return next(new appError('you are not logged in!',401));
         };
 
         // Verify the Token
         const decode = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-        console.log(decode)
+
+        // Check if user still exist
+        const freshUser = await User.findById(decode.id);
+        if(!freshUser){
+            return next(new appError('User does not exist,login again',401));
+        }
         next();
         // res.status(200).json({
         //     status:'sucess',
