2fd55464c5b0bc280e964efc4e80e323bed2f797
Lokman Hossain Razu <89483463+LokmanRazu@users.noreply.github.com>
2022-06-08 02:14:31 +0600

    verify token

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,6 @@
 const appError = require('../utils/error');
+const { promisify } = require('util')
+const jwt = require('jsonwebtoken')
 
 exports.protect = async (req,res,next)=>{
 try{
@@ -10,16 +12,21 @@ try{
         console.log(`I am token:- ${token}`)
         if(!token){
             return next(new appError('you are not logged in!',401));
-        }
-        res.status(200).json({
-            status:'sucess',
-            data:{
-                token
-            }
-        })
+        };
+
+        // Verify the Token
+        const decode = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+        console.log(decode)
+        next();
+        // res.status(200).json({
+        //     status:'sucess',
+        //     data:{
+        //         token
+        //     }
+        // })
     
 }catch(e){
-    console.log(`I am from Protech Middleware: ${e}`);
+    console.log(`I am from Protect Middleware: ${e}`);
         next(e);
 }
 }
