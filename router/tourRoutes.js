26b177359d36dd9eea40c2dccda4d9ba2a297f78
Lokman Hossain Razu <89483463+LokmanRazu@users.noreply.github.com>
2022-06-10 22:39:56 +0600

    User Role

--- router/tourRoutes.js
+++ router/tourRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { getTourController, tourDataController , patchController , deleteTour, } = require('../controller/tourController')
 const { protect,restrictTo } = require('../middleware/authMiddleware')
 
-router.get('/',protect, getTourController)
+router.get('/',protect, getTourController);
 router.post('/',tourDataController)
 router.patch('/:id',protect,restrictTo('admin'),patchController);
 router.delete('/:id' ,deleteTour)
