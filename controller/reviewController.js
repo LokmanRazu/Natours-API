d4c1fe9ee23cf93a1395a4ea0d69841e72ddbe7d
Lokman Hossain Razu <89483463+LokmanRazu@users.noreply.github.com>
2022-06-29 23:15:07 +0600

    GetAllreview

--- controller/reviewController.js
+++ controller/reviewController.js
@@ -2,6 +2,14 @@ const Review = require('./../model/reviewModel')
 
 exports.getAllReview = async (req,res,next)=>{
     try{
+        const reviews = await Review.find();
+        res.status(200).json({
+            status:'success',
+            result: reviews.length,
+            data:{
+                reviews
+            }
+        })
 
     }catch(e){
         console.log(`I am from getAllReview Controller : ${e}`);
